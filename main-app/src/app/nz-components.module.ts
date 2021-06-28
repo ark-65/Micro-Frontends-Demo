42e4ea6dd41161dddddf319b90a25f136d328386
Ark65 <liuwufangzhou@byitgroup.com>
2021-06-28 21:01:31 +0800

    # 更换angular版本

---
+++ main-app/src/app/nz-components.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzFormModule } from 'ng-zorro-antd/form';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [
+    NzInputModule,
+    NzButtonModule,
+    NzGridModule,
+    NzIconModule,
+    NzCheckboxModule,
+    NzFormModule,
+  ],
+})
+export class NzComponentsModule {}
